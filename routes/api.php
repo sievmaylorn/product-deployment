7a8bd43370aaae313f96b163dec483b1d42506af
thecodingbymay <ismemayadnerson04@gmail.com>
2026-09-15 22:22:14 +0700

    fix nginx render deployment

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('products', ProductController::class);
+Route::apiResource('/products', ProductController::class);
